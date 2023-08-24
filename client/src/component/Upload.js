d3e95d2f8871916b85dafa2196ef06112c2208b1
Jaesung Park <richdad6208@gmail.com>
2023-08-24 17:42:27 +0900

    글포스트 서버전송 완료

--- client/src/component/Upload.js
+++ client/src/component/Upload.js
@@ -1,26 +1,40 @@
 import React, { useState } from "react";
-
+import axios from "axios";
 function Upload(props) {
-  const [content, setContent] = useState("");
-  const onSubmit = () => {
-    let tempArr = [...props.contentList];
-    tempArr.push(content);
-    props.setContentList([...tempArr].reverse());
-    setContent("");
+  const [Content, setContent] = useState("");
+  const [Title, setTitle] = useState("");
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    let body = { title: Title, content: Content };
+    axios
+      .post("/api/post", body)
+      .then((response) => {
+        console.log(response, "성공");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
   return (
-    <div class="container py-4">
-      <div class="form-wrapper">
+    <div className="container py-4">
+      <div className="form-wrapper">
         <input
-          class="form__title"
+          className="form__title"
           type="text"
-          value={content}
+          value={Title}
+          onChange={(e) => {
+            setTitle(e.target.value);
+          }}
+        />
+        <textarea
+          className="form__content"
+          value={Content}
           onChange={(e) => {
             setContent(e.target.value);
           }}
-        />
-        <textarea class="form__content"></textarea>
-        <button class="form__button" onClick={onSubmit}>
+        ></textarea>
+        <button className="form__button" onClick={onSubmit}>
           완료
         </button>
       </div>
--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
   app.use(
-    "/api/test",
+    "/api",
     createProxyMiddleware({
       target: "http://localhost:4000",
       changeOrigin: true,
