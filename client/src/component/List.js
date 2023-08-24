a941f46446184b7d3c1bf0fc82134d0844f05c9e
Jaesung Park <richdad6208@gmail.com>
2023-08-24 13:25:04 +0900

    axios 에러해결을 위한 초기화

--- client/src/component/List.js
+++ client/src/component/List.js
@@ -1,21 +1,8 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-const body = { text: "이것은 프론트단에서 백단으로 보내는 작은 쪽지이다" };
+import React from "react";
+
 function List(props) {
-  useEffect(() => {
-    axios
-      .post("/api/test", body)
-      .then((res) => {
-        console.log(setText(res.data.text));
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, []);
-  const [text, setText] = useState("");
   return (
     <div className="container">
-      <h1>{text}</h1>
       {props.contentList.map((item, idx) => {
         return (
           <div className="content-box" key={idx}>
